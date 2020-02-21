c794a0d05dcd2af98867319b7cbf569b4a81515d
hayabusabusa <hayabusabusaSY@gmail.com>
2020-02-21 11:17:23 +0900

    Add input sentence screen class

---
+++ lib/screen/input_sentence.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class InputSentenceScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('変換する'),
+      ),
+    );
+  }
+}
