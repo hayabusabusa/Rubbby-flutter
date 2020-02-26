5a4646e1b46bad40aafc2c495a624156dbc818f5
hayabusabusa <hayabusabusaSY@gmail.com>
2020-02-26 14:17:26 +0900

    Add translation model

---
+++ lib/model/Translation.dart
@@ -0,0 +1,33 @@
+import 'package:equatable/equatable.dart';
+import 'package:rubbby/model/output_type.dart';
+
+class Translation extends Equatable {
+  final String converted;
+  final OutputType outputType;
+
+  const Translation({
+    this.converted,
+    this.outputType,
+  });
+
+  @override
+  List<Object> get props => [
+    converted,
+    outputType,
+  ];
+
+  static Translation fromJson(dynamic json) {
+    return Translation(
+      converted: json['converted'],
+      outputType: _mapStringToOutputType(json['output_type'])
+    );
+  }
+
+  static OutputType _mapStringToOutputType(String input) {
+    switch (input) {
+      case 'hiragana': return OutputType.hiragana;
+      case 'katakana': return OutputType.katakana;
+      default: return OutputType.hiragana;
+    }
+  }
+}
